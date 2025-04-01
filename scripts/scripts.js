1eb65ce4d04135afaf5d66cb1efa84e5d3103c3f
Spring -Git <vaahne@yahoo.com>
2025-04-01 12:06:54 -0600

    Part 3

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,18 +1,76 @@
 // alert("hi");
-
+//  ***********PART 1*******************
+const primeDiv = document.querySelector(".primeDiv");
 var counter = 0;
-
 function recursive(){
     counter ++;
-    console.log(counter);  
-    // 10349
-    // Maximum call stack size exceeded
+    // console.log(counter);  
     recursive();
 }
-
 try{
     recursive();
 
 }catch(e){
     console.error(e.message," counter Value: ",counter);
+}
+
+// ************************ PART 2 ************************
+//  Flatten an array using recursion
+
+function flatArr(arr){
+    let flat = [];
+    arr.forEach(element => {
+       if(Array.isArray(element)){
+            flat = flat.concat(flatArr(element));
+       }else{
+            flat.push(element);
+       }
+    });
+    return flat;
+}
+
+let array = [2,3,[24,4,42,[32,[42]],343,[34,34],43,[3],53],35,[34]];
+
+try{
+    let flatten = flatArr(array);
+    console.log(flatten);
+}catch(e){
+    console.error(e.message);
+}
+
+
+//  ************** PART3 **************************
+
+
+function isPrime(n){
+    for(let j= 2;j<n;j++){
+        if(n%j == 0)
+            return false;
+    }
+    return true;
+}
+let startsAt = 2;
+const number = 10000;
+let primeNums = [];
+
+function renderPrime(){
+    
+     if(startsAt > number){
+        alert("All prime numbers found");
+        return;
+    }
+    if(isPrime(startsAt)){
+        primeDiv.textContent += startsAt + " ,";
+        primeNums.push(startsAt);
+        console.log(startsAt);
+    }
+    startsAt ++;
+    setTimeout(renderPrime,0);
+}
+
+try{
+renderPrime()
+console.log(primeNums);
+}catch(e){
+    console.error(e.message);
 }
